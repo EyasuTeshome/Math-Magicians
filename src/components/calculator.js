8c7dd30ba9535f664bc5384495f5656a7e3fa180
EyasuTeshome <joshtesh91@mail.com>
2022-08-11 21:51:56 +0800

    used calculate.js and operate.js to make the calculator I created in the last part functional

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import calculate from '../logic/calculate';
 
 class Calculator extends React.Component {
   constructor(props) {
@@ -6,42 +7,48 @@ class Calculator extends React.Component {
     this.state = {};
   }
 
+  onClickHandler = (e) => {
+    const result = calculate(this.state, e.target.innerHTML);
+    this.setState(result);
+  }
+
   render() {
+    const { next, total } = this.state;
     return (
       <div className="calculator-container">
         <article className="article-class">
           <div className="result-display">
-            0
+            { next ?? total ?? 0 }
           </div>
         </article>
         <article className="article-class">
-          <button type="button" className="one-fourth-class">AC</button>
-          <button type="button" className="one-fourth-class">+/-</button>
-          <button type="button" className="one-fourth-class">%</button>
-          <button type="button" className="last-one-fourth-class">&#247;</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">AC</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">+/-</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">%</button>
+          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">÷</button>
         </article>
         <article className="article-class">
-          <button type="button" className="one-fourth-class">7</button>
-          <button type="button" className="one-fourth-class">8</button>
-          <button type="button" className="one-fourth-class">9</button>
-          <button type="button" className="last-one-fourth-class">x</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">7</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">8</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">9</button>
+          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">x</button>
         </article>
         <article className="article-class">
-          <button type="button" className="one-fourth-class">4</button>
-          <button type="button" className="one-fourth-class">5</button>
-          <button type="button" className="one-fourth-class">6</button>
-          <button type="button" className="last-one-fourth-class">-</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">4</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">5</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">6</button>
+          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">-</button>
         </article>
         <article className="article-class">
-          <button type="button" className="one-fourth-class">1</button>
-          <button type="button" className="one-fourth-class">2</button>
-          <button type="button" className="one-fourth-class">3</button>
-          <button type="button" className="last-one-fourth-class">+</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">1</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">2</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">3</button>
+          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">+</button>
         </article>
         <article className="article-class">
-          <button type="button" className="first-one-fourth-class">0</button>
-          <button type="button" className="one-fourth-class">.</button>
-          <button type="button" className="last-one-fourth-class">=</button>
+          <button onClick={this.onClickHandler} type="button" className="first-one-fourth-class">0</button>
+          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">.</button>
+          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">=</button>
         </article>
       </div>
     );
