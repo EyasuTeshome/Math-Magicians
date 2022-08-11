dd28c974de84d8020c035511509f2925939f643a
EyasuTeshome <joshtesh91@mail.com>
2022-08-11 23:01:35 +0800

    change calculator.js from class based component to functional component

--- src/components/calculator.js
+++ src/components/calculator.js
@@ -1,58 +1,54 @@
-import React from 'react';
+import React, { useState } from 'react';
 import calculate from '../logic/calculate';
 
-class Calculator extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {};
-  }
+const Calculator = () => {
+  const [stateObj, setStateObj] = useState({ total: '0' });
 
-  onClickHandler = (e) => {
-    const result = calculate(this.state, e.target.innerHTML);
-    this.setState(result);
-  }
+  const onClickHandler = (e) => {
+    const result = calculate(stateObj, e.target.innerHTML);
+    setStateObj(result);
+  };
 
-  render() {
-    const { next, total } = this.state;
-    return (
-      <div className="calculator-container">
-        <article className="article-class">
-          <div className="result-display">
-            { next ?? total ?? 0 }
-          </div>
-        </article>
-        <article className="article-class">
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">AC</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">+/-</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">%</button>
-          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">÷</button>
-        </article>
-        <article className="article-class">
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">7</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">8</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">9</button>
-          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">x</button>
-        </article>
-        <article className="article-class">
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">4</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">5</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">6</button>
-          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">-</button>
-        </article>
-        <article className="article-class">
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">1</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">2</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">3</button>
-          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">+</button>
-        </article>
-        <article className="article-class">
-          <button onClick={this.onClickHandler} type="button" className="first-one-fourth-class">0</button>
-          <button onClick={this.onClickHandler} type="button" className="one-fourth-class">.</button>
-          <button onClick={this.onClickHandler} type="button" className="last-one-fourth-class">=</button>
-        </article>
-      </div>
-    );
-  }
-}
+  return (
+    <div className="calculator-container">
+      <article className="article-class">
+        <div className="result-display">
+          { stateObj.total }
+          { stateObj.operation }
+          { stateObj.next }
+        </div>
+      </article>
+      <article className="article-class">
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">AC</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">+/-</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">%</button>
+        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">÷</button>
+      </article>
+      <article className="article-class">
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">7</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">8</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">9</button>
+        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">x</button>
+      </article>
+      <article className="article-class">
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">4</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">5</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">6</button>
+        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">-</button>
+      </article>
+      <article className="article-class">
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">1</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">2</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">3</button>
+        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">+</button>
+      </article>
+      <article className="article-class">
+        <button onClick={onClickHandler} type="button" className="first-one-fourth-class">0</button>
+        <button onClick={onClickHandler} type="button" className="one-fourth-class">.</button>
+        <button onClick={onClickHandler} type="button" className="last-one-fourth-class">=</button>
+      </article>
+    </div>
+  );
+};
 
 export default Calculator;
